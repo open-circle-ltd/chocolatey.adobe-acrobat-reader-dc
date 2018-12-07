ddff90b92c79c6d44c521d5df445ada6548d5141
teknowledgist <teknowledgist@users.noreply.github.com>
2018-12-07 08:40:26 -0500

    Suggestion for robust overhaul
    This PR results in:
    
    1. An AU script that collects both the latest .exe (MUI) installer and the latest .msp patch.
    2. A package that checks for existing Adobe Reader DC installs and notifies the user of changes or problems.
    3. An install that defaults to no automatic updates without blocking manual checks.
    4. Allows the install command to set the upgrade mode and/or disable the possibility of AdobeARM-based upgrades.
    5. Removes any update block on uninstall.  (important!)
    6. Provides Adobe's suggestion for a fix on encountering a 1603 error.
    7. Logs the msiexec installation and update processes.
    8. A .nuspec with an element order matching that of the Chocolatey default template (for consistency).
    
    Hope you approve!

--- adobereader/update.ps1
+++ adobereader/update.ps1
@@ -1,41 +1,63 @@
 import-module au
 
-$url                    = 'https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html'
-$checksumTypePackageMSP = "SHA512"
+function global:au_GetLatest {
+   # Discover the latest release version
+   $VersionURL  = 'https://helpx.adobe.com/acrobat/release-note/release-notes-acrobat-reader.html'
+   $download_page = Invoke-WebRequest -Uri $VersionURL #-UseBasicParsing -DisableKeepAlive
+   $ReleaseText = $download_page.links | 
+                     Where-Object {$_.innertext -match 'DC.*\([0-9.]+\)'} |
+                     Select-Object -ExpandProperty innertext -First 1
+   $Release = $ReleaseText -replace '.*\(([0-9.]+)\).*','$1'
+   $version = "20$Release"
+   $ReleaseFolder = $Release.replace('.','')
+
+   # Find the MSP for the latest version
+   $FTPbase = 'ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC'
+   $MSPpage = Invoke-WebRequest -Uri "$FTPbase/$ReleaseFolder" -UseBasicParsing -DisableKeepAlive
+   $MUIMSPstub = $MSPpage.rawcontent -split '"' |
+                  Where-Object {$_ -match 'MUI\.msp$'} |
+                  Select-Object -First 1
+   $MSPstub = $MSPpage.rawcontent -split '"' |
+                  Where-Object {$_ -match '\d\.msp$'} |
+                  Select-Object -First 1
+
+   # Find the most-recent EXE
+   $FTPpage = Invoke-WebRequest -Uri $FTPbase -UseBasicParsing -DisableKeepAlive
+   $Folders = $FTPpage.rawcontent -split '[\r\n]' | 
+                  Where-Object {$_ -match '>\d+<'} |
+                  ForEach-Object {$_ -replace '.*>(\d+)<.*','$1'} |
+                  Sort-Object -Descending
+   foreach ($folder in $Folders) {
+      $FolderFiles = Invoke-WebRequest -Uri "$FTPbase/$folder" -UseBasicParsing -DisableKeepAlive
+      $EXEstub = $FolderFiles.rawcontent -split '"' |
+                     Where-Object {$_ -match 'en_US\.exe$'} |
+                     Select-Object -First 1
+      if ($EXEstub) { break }
+   }
+   $MUIstub = $EXEstub -replace 'en_US','MUI'
+
+   return  @{ 
+       Version   = $version
+       URL32     = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/$MUIstub"
+       MUIMSPurl = "$FTPbase/$MUIMSPstub"
+   }
+}
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1'   = @{
-            "(^\s*[$]*urlPackageMSP\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(^\s*[$]*checksumPackageMSP\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
-            "(^\s*[$]*checksumTypePackage\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-        }; 
-    }
+   @{
+      'tools\chocolateyInstall.ps1' = @{
+         "(^[$]MUIurl\s*=\s*)('.*')"         = "`$1'$($Latest.URL32)'"
+         "(^[$]MUIchecksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum32)'"
+         "(^[$]MUImspURL\s*=\s*)('.*')"      = "`$1'$($Latest.MUIMSPurl)'"
+         "(^[$]MUImspChecksum\s*=\s*)('.*')" = "`$1'$($Latest.MUImspChecksum)'"
+      } 
+   }
 }
 
-function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $url -UseBasicParsing -DisableKeepAlive
-
-    $reLatestbuild = "(.*>DC.*)"
-    $download_page.RawContent -imatch $reLatestbuild
-    $latestbuild   = $Matches[0]
-
-    $reVersion   = "(\d+)(.)(\d+)(.)(\d+)"
-    $latestbuild -imatch $reVersion
-    $version     = "20$($Matches[0])"
-    $urlVersion  = $Matches[0].Replace(".", "")
-    
-    $urlPackageMSP = "http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/$($urlVersion)/AcroRdrDCUpd$($urlVersion)_MUI.msp"
-
-    return  @{ 
-        URL32          = $urlPackageMSP;
-        ChecksumType32 = $checksumTypePackageMSP;
-        Version        = $version
-    }
+function global:au_BeforeUpdate() {
+   $Latest.MUImspChecksum = Get-RemoteChecksum $Latest.MUIMSPurl
 }
 
-function global:au_AfterUpdate ($Package) {
-    Set-DescriptionFromReadme $Package -SkipFirst 3
-}
+Update-Package -NoCheckChocoVersion
 
-update
+
